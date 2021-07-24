ba0d952629c32da51bdec623ed2a4b2aa184eef2
David Basile Filho <39467981+DavidBF16@users.noreply.github.com>
2021-07-24 10:58:23 -0300

    Add files via upload

---
+++ save_system.gd
@@ -0,0 +1,47 @@
+class_name EzSaveSystem
+# Make sure the directory starts with "user://"
+# to get a persistent data path across all platforms
+
+
+static func save(data, directory := "user://saves/", file_name := "save", 
+		extension := ".sav", password := "PASSWORD") -> void:
+	
+	var dir := Directory.new()
+	var path := directory + file_name + extension
+	var file := File.new()
+	
+	if not dir.dir_exists(directory):
+		dir.make_dir_recursive(directory)
+	
+	var error := file.open_encrypted_with_pass(path, File.WRITE, password)
+	if error == OK:
+		file.store_string(var2str(data))
+		file.close()
+
+
+static func load(directory := "user://saves/", file_name := "save",
+		extension := ".sav", password := "PASSWORD"):
+	var path = directory + file_name + extension
+	var file := File.new()
+	if file.file_exists(path):
+		var error := file.open_encrypted_with_pass(path,
+			File.READ, password)
+		if error == OK:
+			var data = str2var(file.get_as_text())
+			file.close()
+			return data
+	else:
+		# File does not exist
+		return null
+
+
+static func file_exists(directory := "user://saves/", file_name := "save",
+	extension := ".sav") -> bool:
+	var file := File.new()
+	return file.file_exists(directory + file_name + extension)
+
+
+static func directory_exists(directory := "user://saves/") -> bool:
+	var dir := Directory.new()
+	return dir.dir_exists(directory)
+
